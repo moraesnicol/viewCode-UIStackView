9f02849f307484e4149955d0fcb6ade9f1e72dd5
Gabriel <marcos.santos@e-deploy.com.br>
2021-08-20 18:11:13 -0300

    Initial Commit

---
+++ viewCode-UIStackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewCode-UIStackView
+//
+//  Created by Gabriel on 20/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
